3e92c45aa545c024bede3fc86a5eb8dd68bdf567
Maxence Maire <maire.maxence@gmail.com>
2024-12-06 03:44:04 +0100

    implement loading of GLTF model

--- src/asset.rs
+++ src/asset.rs
@@ -1,11 +1,18 @@
 use crate::graphics;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
+
+type TextureArrayId = usize;
+type TextureId = usize;
+type MeshId = usize;
+type MaterialId = usize;
+type ModelId = usize;
 
 pub struct AssetLoader {
     pub mesh_map: graphics::mesh::MeshMap,
     pub texture_arrays: graphics::texture::TextureArrays,
-    pub texture_dictionary: HashMap<String, (usize, usize)>,
+    pub texture_dictionary: HashMap<String, (TextureArrayId, TextureId)>,
     pub material_map: graphics::material::MaterialMap,
+    pub model_map: ModelMap,
 }
 
 impl AssetLoader {
@@ -15,10 +22,11 @@ impl AssetLoader {
             texture_arrays: graphics::texture::TextureArrays::new(),
             texture_dictionary: HashMap::new(),
             material_map: graphics::material::MaterialMap::default(),
+            model_map: ModelMap::default(),
         }
     }
 
-    pub fn load_gltf_model<P>(&mut self, path: P) -> Result<(), GltfError>
+    pub fn load_gltf_model<P>(&mut self, path: P) -> Result<ModelId, GltfError>
     where
         P: AsRef<std::path::Path>,
     {
@@ -27,10 +35,17 @@ impl AssetLoader {
         let path = path.as_ref();
         let canonicalized_path = path.canonicalize().unwrap();
 
+        if let Some(&model_id) = self.model_map.map.get(canonicalized_path.to_str().unwrap()) {
+            return Ok(model_id);
+        }
+
         // TODO: error message. No parent path.
         let directory_path = path.parent().unwrap();
 
-        let mut load_texture = |texture_path: &std::path::Path| -> Result<(usize, usize), graphics::texture::TextureError> {
+        let mut load_texture = |texture_path: &std::path::Path| -> Result<
+            (TextureArrayId, TextureId),
+            graphics::texture::TextureError,
+        > {
             let texture_path_str = texture_path.to_str().unwrap();
 
             let (texture_array_id, texture_id) = if let Some((texture_array_id, texture_id)) =
@@ -40,10 +55,12 @@ impl AssetLoader {
             } else {
                 let texture_data = std::fs::read(texture_path_str).unwrap();
                 let ktx2_reader = ktx2::Reader::new(texture_data).unwrap();
-                let (format, texture_id) =
-                    self.texture_arrays.add(texture_path_str.to_string(), ktx2_reader)?;
+                let (format, texture_id) = self
+                    .texture_arrays
+                    .add(texture_path_str.to_string(), ktx2_reader)?;
                 let texture_array_id = format.id();
-                self.texture_dictionary.insert(texture_path_str.to_string(), (texture_array_id, texture_id));
+                self.texture_dictionary
+                    .insert(texture_path_str.to_string(), (texture_array_id, texture_id));
                 (texture_array_id, texture_id)
             };
 
@@ -52,107 +69,182 @@ impl AssetLoader {
 
         let gltf::Gltf { document, .. } = gltf::Gltf::open(path).unwrap();
         let buffers = gltf::import_buffers(&document, Some(directory_path), None).unwrap();
-        for mesh in document.meshes() {
-            for (i, primitive) in mesh.primitives().enumerate() {
-                let name = format!("{}/{}", mesh.name().unwrap(), i);
+        let mut nodes = Vec::new();
+        let mut stack = VecDeque::new();
 
-                let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+        let default_scene = document.default_scene().unwrap();
+        for gltf_node in default_scene.nodes() {
+            stack.push_back(gltf_node);
+        }
 
-                let gltf_material = primitive.material();
-                let pbr_metallic_roughness = gltf_material.pbr_metallic_roughness();
-                // TODO: error message. Object must have a base color texture.
-                let base_color_texture_information =
-                    pbr_metallic_roughness.base_color_texture().unwrap();
+        while let Some(gltf_node) = stack.pop_front() {
+            let mut node = Node {
+                name: gltf_node.name().map(|s| s.to_string()),
+                object_group: None,
+                children: Vec::new(),
+            };
 
-                let (base_color_texture_array_id, base_color_texture_id) =
-                    if let gltf::image::Source::Uri { uri, .. } =
-                        base_color_texture_information.texture().source().source()
-                    {
-                        // TODO: error message. error if invalid file path.
-                        let texture_path = directory_path.join(uri).canonicalize().unwrap();
-                        load_texture(&texture_path)?
-                    } else {
-                        // TODO: error message. Expected URI source.
-                        panic!();
-                    };
-
-                let tex_coords_set_index = base_color_texture_information.tex_coord();
-                // TODO: error message.
-                let tex_coords = if let gltf::mesh::util::ReadTexCoords::F32(tex_coords_iter) =
-                    reader.read_tex_coords(tex_coords_set_index).unwrap()
-                {
-                    tex_coords_iter
-                } else {
-                    // TODO: error message. Expected f32 tex coords.
-                    panic!();
+            if let Some(gltf_mesh) = gltf_node.mesh() {
+                let mut object_group = ObjectGroup {
+                    objects: Vec::new(),
                 };
 
-                let (normal_texture_array_id, normal_texture_id) =
-                    if let gltf::image::Source::Uri { uri, .. } = gltf_material
-                        .normal_texture()
-                        .unwrap()
-                        .texture()
-                        .source()
-                        .source()
+                for (i, primitive) in gltf_mesh.primitives().enumerate() {
+                    let name = format!("{}/{}", gltf_mesh.name().unwrap(), i);
+                    let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+
+                    let gltf_material = primitive.material();
+                    let pbr_metallic_roughness = gltf_material.pbr_metallic_roughness();
+                    // TODO: error message. Object must have a base color texture.
+                    let base_color_texture_information =
+                        pbr_metallic_roughness.base_color_texture().unwrap();
+
+                    let (base_color_texture_array_id, base_color_texture_id) =
+                        if let gltf::image::Source::Uri { uri, .. } =
+                            base_color_texture_information.texture().source().source()
+                        {
+                            // TODO: error message. error if invalid file path.
+                            let texture_path = directory_path.join(uri).canonicalize().unwrap();
+                            load_texture(&texture_path)?
+                        } else {
+                            // TODO: error message. Expected URI source.
+                            panic!();
+                        };
+
+                    let tex_coords_set_index = base_color_texture_information.tex_coord();
+                    // TODO: error message.
+                    let tex_coords = if let gltf::mesh::util::ReadTexCoords::F32(tex_coords_iter) =
+                        reader.read_tex_coords(tex_coords_set_index).unwrap()
                     {
-                        // TODO: error message. error if invalid file path.
-                        let texture_path = directory_path.join(uri).canonicalize().unwrap();
-                        load_texture(&texture_path)?
+                        tex_coords_iter
                     } else {
-                        // TODO: error message. Expected URI source.
+                        // TODO: error message. Expected f32 tex coords.
                         panic!();
                     };
 
-                let material = graphics::material::Material::new(
-                    base_color_texture_array_id,
-                    base_color_texture_id,
-                    normal_texture_array_id,
-                    normal_texture_id,
-                );
+                    let (normal_texture_array_id, normal_texture_id) =
+                        if let gltf::image::Source::Uri { uri, .. } = gltf_material
+                            .normal_texture()
+                            .unwrap()
+                            .texture()
+                            .source()
+                            .source()
+                        {
+                            // TODO: error message. error if invalid file path.
+                            let texture_path = directory_path.join(uri).canonicalize().unwrap();
+                            load_texture(&texture_path)?
+                        } else {
+                            // TODO: error message. Expected URI source.
+                            panic!();
+                        };
 
-                let material_index = self.material_map.add(material);
+                    let material = graphics::material::Material::new(
+                        base_color_texture_array_id,
+                        base_color_texture_id,
+                        normal_texture_array_id,
+                        normal_texture_id,
+                    );
 
-                // TODO: add metallic and roughness information to material.
-                let metallic_roughness_texture_information =
-                    pbr_metallic_roughness.metallic_roughness_texture().unwrap();
+                    let material_index = self.material_map.add(material);
 
-                let vertex_positions = reader.read_positions().unwrap(); // TODO: error message.
-                let vertex_normals = reader.read_normals().unwrap(); // TODO: error message.
-                let vertex_tangents = reader.read_tangents().unwrap(); // TODO: error message.
+                    // TODO: add metallic and roughness information to material.
+                    let metallic_roughness_texture_information =
+                        pbr_metallic_roughness.metallic_roughness_texture().unwrap();
 
-                let mut vertices = Vec::with_capacity(vertex_positions.len());
-                for (position, tex_coord, normal, tangent) in itertools::izip!(
-                    vertex_positions,
-                    tex_coords,
-                    vertex_normals,
-                    vertex_tangents
-                ) {
-                    vertices.push(graphics::mesh::Vertex::new(
-                        position, tex_coord, normal, tangent,
-                    ));
+                    let vertex_positions = reader.read_positions().unwrap(); // TODO: error message.
+                    let vertex_normals = reader.read_normals().unwrap(); // TODO: error message.
+                    let vertex_tangents = reader.read_tangents().unwrap(); // TODO: error message.
+
+                    let mut vertices = Vec::with_capacity(vertex_positions.len());
+                    for (position, tex_coord, normal, tangent) in itertools::izip!(
+                        vertex_positions,
+                        tex_coords,
+                        vertex_normals,
+                        vertex_tangents
+                    ) {
+                        vertices.push(graphics::mesh::Vertex::new(
+                            position, tex_coord, normal, tangent,
+                        ));
+                    }
+
+                    let indices = reader
+                        .read_indices()
+                        .unwrap() // TODO: error message.
+                        .into_u32()
+                        .collect::<Vec<_>>();
+
+                    let mesh_index = self.mesh_map.push(
+                        format!("{}#{}", canonicalized_path.to_str().unwrap(), name),
+                        vertices,
+                        indices,
+                    );
+
+                    object_group.objects.push((mesh_index, material_index));
                 }
 
-                let indices = reader
-                    .read_indices()
-                    .unwrap() // TODO: error message.
-                    .into_u32()
-                    .collect::<Vec<_>>();
-
-                let mesh_index = self.mesh_map.push(
-                    format!("{}#{}", canonicalized_path.to_str().unwrap(), name),
-                    vertices,
-                    indices,
-                );
-
-                println!("{:?}", (mesh_index, material_index));
+                node.object_group = Some(object_group);
             }
+
+            for child in gltf_node.children() {
+                stack.push_back(child);
+                node.children.push(nodes.len());
+            }
+
+            nodes.push(node);
         }
+
+        let model = Model { nodes };
 
         println!("{:#?}", self.texture_dictionary);
         println!("{:#?} {:#?}", self.mesh_map.meshes, self.mesh_map.map);
         println!("{:#?}", self.material_map);
+        println!("{:#?}", model);
 
-        Ok(())
+        let model_index = self
+            .model_map
+            .add(path.to_str().unwrap().to_string(), model);
+
+        Ok(model_index)
+    }
+}
+
+#[derive(Debug)]
+pub struct Model {
+    nodes: Vec<Node>,
+}
+
+#[derive(Debug)]
+pub struct Node {
+    name: Option<String>,
+    object_group: Option<ObjectGroup>,
+    children: Vec<usize>,
+}
+
+type Object = (MeshId, MaterialId);
+
+#[derive(Debug)]
+pub struct ObjectGroup {
+    objects: Vec<Object>,
+}
+
+#[derive(Default)]
+pub struct ModelMap {
+    pub models: Vec<Model>,
+    pub map: HashMap<String, usize>,
+}
+
+impl ModelMap {
+    pub fn add(&mut self, name: String, model: Model) -> usize {
+        if let Some(&model_index) = self.map.get(&name) {
+            return model_index;
+        }
+
+        let model_index = self.models.len();
+
+        self.models.push(model);
+        self.map.insert(name, model_index);
+
+        model_index
     }
 }
 
