2b524304370ff061d97fe658be312b33f034ff4f
Maxence Maire <maire.maxence@gmail.com>
2024-12-14 17:27:36 +0100

    fix default GlobalTransform, Transform

--- src/ecs/component/transform.rs
+++ src/ecs/component/transform.rs
@@ -2,8 +2,14 @@ use bevy_ecs::component::Component;
 use std::ops::{Deref, DerefMut};
 
 #[repr(C)]
-#[derive(Component, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable, Default, Debug)]
+#[derive(Component, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable, Debug)]
 pub struct GlobalTransform(pub [f32; 12]);
+
+impl Default for GlobalTransform {
+    fn default() -> Self {
+        Self(glam::Affine3A::IDENTITY.to_cols_array())
+    }
+}
 
 impl Deref for GlobalTransform {
     type Target = [f32; 12];
@@ -19,9 +25,19 @@ impl DerefMut for GlobalTransform {
     }
 }
 
-#[derive(Component, Default, Debug)]
+#[derive(Component, Debug)]
 pub struct Transform {
     pub translation: glam::Vec3,
     pub rotation: glam::Quat,
     pub scale: glam::Vec3,
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self {
+            translation: glam::Vec3::ZERO,
+            rotation: glam::Quat::IDENTITY,
+            scale: glam::Vec3::ONE,
+        }
+    }
 }
